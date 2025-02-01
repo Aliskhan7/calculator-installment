4904b7cf1b0012b0044add758fb3c4af374dd451
aliskhangazamatov <ag@farcana.com>
2025-02-01 17:30:14 +0300

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Calculator from "./components/shared/Calculator";
 
 function App() {
   return (
-      <div className="sm:container mx-auto">
+      <div className="sm:container mx-auto relative w-full h-screen">
         <Calculator/>
       </div>
   );
--- src/components/shared/Calculator.jsx
+++ src/components/shared/Calculator.jsx
@@ -46,14 +46,13 @@ const Calculator = () => {
     };
 
     return (
-        <div>
-            <h2>Калькулятор рассрочки:</h2>
-            <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-
+        <div className='w-1/2 mx-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+            <h2 className='text-4xl text-center pt-5 font-bold'>Калькулятор рассрочки:</h2>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-12">
                 <div className="flex flex-col gap-2">
                     <label>Сумма рассрочки:</label>
                     <input
-                        className="border-2 border-gray-200"
+                        className="border-2 border-gray-200 px-2 py-2 rounded-md "
                         type="number"
                         value={loanAmount}
                         onChange={(e) => setLoanAmount(Number(e.target.value))}
@@ -63,22 +62,24 @@ const Calculator = () => {
                 <div className="flex flex-col gap-2">
                     <label>Срок рассрочки (месяцев):</label>
                     <input
-                        className="border-2 border-gray-200"
+                        className="border-2 border-gray-200 px-2 py-2 rounded-md "
                         type="number"
                         value={loanTerm}
                         onChange={(e) => setLoanTerm(e.target.value)}
                     />
                 </div>
 
-                <button className="border-2 border-gray-200" type="submit">
+                <button className="w-fit mx-auto py-2 px-4 border-2 border-gray-200 rounded-md" type="submit">
                     Рассчитать
                 </button>
             </form>
 
             {/* Выводим все необходимые данные: */}
-            <h3>Сумма рассрочки (без процентов): {loanAmount} руб.</h3>
-            <h3>Полная сумма (с процентами): {totalWithInterest} руб.</h3>
-            <h3>Ежемесячный платеж: {monthlyPayment} руб.</h3>
+            <div className='flex flex-col gap-2 bg-amber-300/50 p-3 rounded-lg mt-5'>
+                <h3>Сумма рассрочки (без процентов): {loanAmount} руб.</h3>
+                <h3>Полная сумма (с процентами): {totalWithInterest} руб.</h3>
+                <h3>Ежемесячный платеж: {monthlyPayment} руб.</h3>
+            </div>
         </div>
     );
 };
