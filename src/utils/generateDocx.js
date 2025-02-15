5493c066a1b73a325ea52d8fbb26e071b6e2a6ba
aliskhangazamatov <ag@farcana.com>
2025-02-15 20:19:12 +0300

    add docs

---
+++ src/utils/generateDocx.js
@@ -0,0 +1,46 @@
+import PizZip from "pizzip";
+import Docxtemplater from "docxtemplater";
+import { saveAs } from "file-saver";
+import {formatNumber} from "./formatters";
+import templateDocx from "../static/documents/template.docx"
+
+// Функция загрузки шаблона и подстановки данных
+export const generateWordFromTemplate = async (fullName,
+                                               age,
+                                               phone,
+                                               passportNumber,
+                                               passportSeries,
+                                               address, loanAmount, downPayment, totalWithInterest, monthlyPayment, ) => {
+    try {
+        // 1️⃣ Загружаем шаблон Word (из папки public/static)
+        const response = await fetch(templateDocx);
+        const templateArrayBuffer = await response.arrayBuffer();
+
+        // 2️⃣ Загружаем файл DOCX в виде ZIP-архива
+        const zip = new PizZip(templateArrayBuffer);
+        const doc = new Docxtemplater(zip);
+
+        // 3️⃣ Подставляем реальные значения в плейсхолдеры
+        doc.setData({
+            fullName,
+            age,
+            phone,
+            passportNumber,
+            passportSeries,
+            address,
+            loanAmount: formatNumber(loanAmount.toLocaleString("ru-RU")),
+            downPayment: formatNumber(downPayment.toLocaleString("ru-RU")),
+            totalWithInterest: formatNumber(totalWithInterest.toLocaleString("ru-RU")),
+            monthlyPayment: formatNumber(monthlyPayment.toLocaleString("ru-RU")),
+        });
+
+        // 4️⃣ Генерируем документ с новыми данными
+        doc.render();
+
+        // 5️⃣ Создаем Blob и скачиваем обновленный DOCX
+        const updatedDoc = doc.getZip().generate({ type: "blob" });
+        saveAs(updatedDoc, "calculator-result.docx");
+    } catch (error) {
+        console.error("Ошибка при создании документа:", error);
+    }
+};
